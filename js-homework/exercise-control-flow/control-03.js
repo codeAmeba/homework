b7d7cf2a47625d86bc401878045e0b6ed2287557
jeongsooyoung <code.ameba@gmail.com>
2019-05-12 23:57:33 +0900

    js homework

--- js-homework/exercise-control-flow/control-03.js
+++ js-homework/exercise-control-flow/control-03.js
@@ -1,8 +1,9 @@
 // for문을 사용하여 0부터 10미만의 정수 중에서 짝수만을 작은 수부터 문자열로 출력
 
+// 가로로 출력 실패
 for (let i = 0; i < 10; i++) {
   if (i % 2 === 0) {
-    let j = i.toString();
+    const j = i.toString();
     console.log(typeof j, j);
   }
 }
--- js-homework/exercise-control-flow/control-06.js
+++ js-homework/exercise-control-flow/control-06.js
@@ -1,12 +1,13 @@
 // while문을 사용하여 0부터 10 미만의 정수 중에서 홀수만을 큰수부터 출력
 
-// 순서 반전 실패
-let i = 0;
+let i = 10;
 
-while (i < 10) {
-  i++;
+while (i > 0) {
+  i -= 1;
   if (i % 2 === 1) {
+    if (i === 0) {
+      break;
+    }
     console.log(i);
-    if (i === 10) break;
   }
 }
